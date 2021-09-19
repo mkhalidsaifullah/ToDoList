bd034c580bbffee1417129705a102bd7436b0dcf
Muhammad Khalid سیف اللہ <83535386+mkhalidsaifullah@users.noreply.github.com>
2021-09-19 21:23:29 +0500

    useref in login form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import Login from "./components/Login";
 
 function App() {
+  const [user, setUser] = useState("");
+  const userHandler = (user) => {
+    setUser(user);
+  };
+  
+  console.log(user);
+
   return (
     <>
-      <Login />
+      <Login userName={userHandler} />
     </>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,29 @@
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import { useState } from "react";
+import { useRef } from "react";
 
-const Login = () => {
-  const [name, setName] = useState("");
-  let nameHandler = (e) => {
-    setName(e.target.value);
-  };
-
+const Login = (props) => {
+  const nameRef = useRef("");
   const formHandler = (e) => {
     e.preventDefault();
-    console.log(name);
+    const name = nameRef.current.value;
+
+    if (name.trim().length === 0) {
+      alert("name field is empty");
+      return;
+    }
+
+    /*  console.log(name); */
+    props.userName(name);
+    nameRef.current.value = "";
   };
 
   return (
     <div>
-      {!name && (
-        <Card sx={{ minWidth: 275 }}>
-          <CardContent>
-            <Box
-              component="form"
-              sx={{
-                "& > :not(style)": { m: 1, width: "25ch" },
-              }}
-              noValidate
-              autoComplete="off"
-              onSubmit={formHandler}
-            >
-              <TextField
-                id="outlined-basic"
-                label="Name"
-                variant="outlined"
-                onChange={nameHandler}
-              />
-              <Button variant="text">Submit</Button>
-            </Box>
-          </CardContent>
-        </Card>
-      )}
-      {name && <h1>WellCome {name}</h1>}
+      {
+        <form onSubmit={formHandler}>
+          <input type="text" placeholder="name" ref={nameRef} />
+          <input type="submit" value="Submit" />
+        </form>
+      }
     </div>
   );
 };
