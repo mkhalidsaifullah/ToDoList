4d6da9b3aa04d3210c6dc4104e8fc0ee4a8cb243
Muhammad Khalid سیف اللہ <83535386+mkhalidsaifullah@users.noreply.github.com>
2021-09-20 19:43:54 +0500

    2 todo page (#2)
    * useref in login form
    
    * login page and well come message done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import Login from "./components/Login";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [user, setUser] = useState("");
+  const userHandler = (user) => {
+    setUser(user);
+  };
+
+  console.log(user);
+
   return (
     <>
-      <Login />
+      {!user && <Login userName={userHandler} />}
+      {user && <TodoList userName={user} />}
     </>
   );
 }
